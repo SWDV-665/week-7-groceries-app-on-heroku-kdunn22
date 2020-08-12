cfe2a2370879553016d217b567ba0c0cdd1200ce
Katelynn Dunn <kdunn2@live.maryville.edu>
2020-08-11 20:20:21 -0400

    Commit 2

---
+++ Groceries/src/pages/home/home.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+  items = [];
+  errorMessage: string;
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider, public socialSharing: SocialSharing) {
+    dataService.dataChanged$.subscribe((dataChanged: boolean) => {
+      this.loadItems();
+    });
+  }
+
+  ionViewDidLoad(){
+    this.loadItems();
+  }
+  
+  loadItems() {
+    this.dataService.getItems()
+      .subscribe(
+        items => this.items = items,
+        error => this.errorMessage = <any>error);
+  }
+
+  removeItem(id) {
+    this.dataService.removeItem(id);  
+  }
+
+  shareItem(item, index) {
+    console.log("Sharing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Sharing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    let message = "Grocery Item - Name: " + item.name + " - Quantity: " + item.quantity;
+    let subject = "Shared via Groceries app";
+
+    this.socialSharing.share(message, subject).then(() => {
+      // Sharing via email is possible
+      console.log("Shared successfully!");
+    }).catch((error) => {
+      console.error("Error while sharing ", error);
+    });    
+
+  
+
+    
+  }
+
+  editItem(item, index) {
+    console.log("Edit Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Editing Item - ' + index + " ...",
+      duration: 5000
+    });
+    toast.present();
+    this.inputDialogService.showPrompt(item, index);
+  }  
+
+  addItem(item) {
+    console.log("Adding Item");
+    this.inputDialogService.showPrompt();
+    
+  }
+}
+
---
+++ Groceries/src/providers/groceries-service/groceries-service.ts
@@ -0,0 +1,78 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { map, catchError } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+/*
+  Generated class for the GroceriesServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class GroceriesServiceProvider {
+
+  items: any = [];
+
+  dataChanged$: Observable<boolean>;
+
+  private dataChangeSubject: Subject<boolean>;
+
+  baseURL = "https://groceries-server-two.herokuapp.com"
+
+  constructor(public http: HttpClient) {
+    console.log('Hello GroceriesServiceProvider Provider');
+
+    this.dataChangeSubject = new Subject<boolean>();
+    this.dataChanged$ = this.dataChangeSubject.asObservable();
+  }
+
+  getItems(): Observable<object[]> {
+    return this.http.get(this.baseURL + '/api/groceries').pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const err = error || '';
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  removeItem(item) {
+    console.log("#### Remove Item - id = ", item);
+    this.http.delete("https://groceries-server-two.herokuapp.com/api/groceries/" + item._id).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+    });
+  }
+
+  addItem(item) {
+    
+    this.http.post(this.baseURL + "/api/groceries?" + "name=" + item.name + "&quantity=" + item.quantity, item).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+    });
+  }
+
+  editItem(item, index) {
+    console.log("Editing item ", item);
+    this.http.put(this.baseURL + "/api/groceries/" + item._id + "?Name=" + item.name + "&Quantity=" + item.quantity, item).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+    });
+  }
+}
